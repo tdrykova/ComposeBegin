bbc6d910587c326780dfd4bebf81b6b4ccb7191d
tdrykova <tdrykova@edu.hse.ru>
2024-06-28 17:25:50 +0300

    unit 1 task 03 (grid)

--- app/src/main/java/com/tatry/composebegin01/MainActivity.kt
+++ app/src/main/java/com/tatry/composebegin01/MainActivity.kt
@@ -4,20 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.tatry.composebegin01.ui.theme.ComposeBegin01Theme
 
 class MainActivity : ComponentActivity() {
@@ -26,58 +31,126 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeBegin01Theme {
-                Paper()
+                Grid()
             }
         }
     }
 }
 
 @Composable
-fun Paper(modifier: Modifier = Modifier) {
-    Column(modifier) {
-        Image(
+fun Grid(modifier: Modifier = Modifier) {
+    Column(modifier.fillMaxSize()) {
+        Row(
             modifier = Modifier
-                .fillMaxWidth(),
-            painter = painterResource(id = R.drawable.bg_compose_background),
-            contentDescription = null
-        )
-
-        Text(
-            text = stringResource(R.string.paper_title),
-            fontSize = 24.sp,
-            modifier = Modifier.padding(
-                start = 16.dp,
-                end = 16.dp,
-                top = 16.dp,
-                bottom = 16.dp
+                .fillMaxWidth()
+                .weight(1f),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Card(
+                title = stringResource(R.string.text_composable_title),
+                description = stringResource(R.string.text_composable_description),
+                color = 0xFFEADDFF,
+                pos = 0
             )
-        )
 
-        Text(
-            text = stringResource(R.string.part1),
-            modifier = Modifier
-                .padding(start = 16.dp, end = 16.dp),
-            textAlign = TextAlign.Justify,
-            fontSize = TextUnit.Unspecified
-        )
+            Card(
+                title = stringResource(R.string.image_composable_title),
+                description = stringResource(R.string.img_desc),
+                color = 0xFFD0BCFF,
+                pos = 1
+            )
+        }
 
-        Text(
-            text = stringResource(R.string.part2),
-            fontSize = TextUnit.Unspecified,
-            modifier = Modifier.padding(16.dp),
-            textAlign = TextAlign.Justify
-        )
+        Row( modifier = Modifier
+            .fillMaxWidth()
+            .weight(1f),
+                verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Card(
+                title = stringResource(R.string.row_composable_title),
+                description = stringResource(R.string.row_desc),
+                color = 0xFFB69DF8,
+                pos = 2
+            )
+
+            Card(
+                title = stringResource(R.string.image_composable_title),
+                description = stringResource(R.string.img_desc),
+                color = 0xFFF6EDFF,
+                pos = 3
+            )
+
+        }
     }
+
+}
+
+@Composable
+fun Card(title: String, description: String, color: Long, pos: Int) {
+    when (pos) {
+        0, 2 -> {
+            Box(modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(0.5F)
+                .background(Color(color)),
+                contentAlignment = Alignment.Center) {
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = title,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(bottom = 16.dp)
+                    )
+
+                    Text(
+                        text = description,
+                        textAlign = TextAlign.Justify
+                    )
+                }
+            }
+        }
+        1, 3 -> {
+            Box(modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxSize()
+                .background(Color(color)),
+                contentAlignment = Alignment.Center) {
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = title,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(bottom = 16.dp)
+                    )
+
+                    Text(
+                        text = description,
+                        textAlign = TextAlign.Justify
+                    )
+                }
+            }
+        }
+    }
+
 
 }
 
 @Preview(
     showBackground = true,
     showSystemUi = true
-    )
+)
 @Composable
 fun GreetingPreview() {
     ComposeBegin01Theme {
-        Paper()
+        Grid()
     }
 }
